4262a4c2e8de8e397e1782ff44b6cce64fbaf669
administrator <administrator@administrator-Latitude-5490>
2023-08-21 16:19:42 +0530

    first project

---
+++ src/com/ty/Sample.java
@@ -0,0 +1,14 @@
+package com.ty;
+
+public class Sample {
+	public static void main(String[] args) {
+		String name="Madhu";
+		if(name=="Madhu") {
+			System.out.println("Completed  Work....");
+		}
+		else {
+			System.out.println("Doing Work....");
+		}
+	}
+
+}
